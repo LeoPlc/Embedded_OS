b7282136de849892c301510fa76c7cb4976d9132
Leonardo <120414762+LeoPlc@users.noreply.github.com>
2023-10-04 14:07:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
-int main(){
+#include <stdio.h>
 
-    
+int main() {
+    char cpuinfo;
+    FILE *fp = fopen("/proc/xcpuinfo", "r");
+
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening file\n");
+    } else {
+        while ((cpuinfo = fgetc(fp)) != EOF) {
+            printf("%c", cpuinfo);      
+        }
+        fclose(fp);
+    }
+
     return 0;
 }
