292d0df8a9db76e496f0d0c2dd299f4d2947550f
Leonardo <palaric.leo@gmail.com>
2023-12-04 08:18:50 +0100

    vite fais

--- Calcul_intensif/td/signalToolbox/src/global.h
+++ Calcul_intensif/td/signalToolbox/src/global.h
@@ -1,10 +1,10 @@
-#ifndef FT_GLOBAL_H
-#define FT_GLOBAL_H
+#ifndef FT_GLOBAL_H 
+#define FT_GLOBAL_H 
 
 #include <cstddef>
 #include <complex>
 
-constexpr size_t N = 256;
-typedef std::complex<double> Complex;
+constexpr size_t N = 256;               // Taille des tableaxu qui seront acceptés
+typedef std::complex<double> Complex;   // Alias de type désignant les nombres complexes afin que l'écriture soit plus compacte
 
 #endif //FT_GLOBAL_H
